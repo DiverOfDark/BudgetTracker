ca26100a24ef2f0de263c2ecebb31c554e441eda
Kirill Orlov <i@kirillorlov.pro>
2019-03-04 02:22:32 +0300

    Do not set category when payment category matched from another payment

--- BudgetTracker/Services/SpentCategoryProcessor.cs
+++ BudgetTracker/Services/SpentCategoryProcessor.cs
@@ -43,10 +43,6 @@ namespace BudgetTracker.Services
                         p.Debt = matchedPayments[p.What].Debt;
                         p.Category = matchedPayments[p.What].Category;
                         
-                        if (p.Category.Kind != PaymentKind.Unknown)
-                        {
-                            p.Kind = p.Category.Kind;
-                        }
                         continue;
                     }
                     
