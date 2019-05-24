cae11d1d0bb2f508577589d1c1ecf11ac14d85fc
Kirill Orlov <i@kirillorlov.pro>
2019-05-25 01:52:54 +0300

    Working on metadata index page

--- BudgetTracker.Client/src/App.svelte
+++ BudgetTracker.Client/src/App.svelte
@@ -1,10 +1,16 @@
-<script>
+<script lang="ts">
+  //@ts-ignore
   import { Router, Route } from 'svero';
 
-  import BTTable from './pages/BTTable.svelte';
+  import TableIndex from './pages/table/index.svelte';
+  import MetadataIndex from './pages/metadata/index.svelte';
   import NotFound from './pages/NotFound.svelte';
   import Footer from './components/Footer.svelte'
-  import Nav from './components/Nav.svelte'
+  import Nav from './components/Nav.svelte';
+  import Tooltip from './components/Tooltip.svelte';
+
+  // used in templating:
+  Router; Route; TableIndex; MetadataIndex; NotFound; Footer; Nav; Tooltip; 
 </script>
 
 <div class="page">
@@ -13,9 +19,11 @@
 		<div class="page-content">
       <Router>
         <Route path="*" component={NotFound} />
-        <Route path="/Table" component={BTTable} />
+        <Route path="/Table" component={TableIndex} />
+        <Route path="/Metadata" component={MetadataIndex} />
       </Router>
 		</div>
 	</div>
 	<Footer />
+  <Tooltip />
 </div>
