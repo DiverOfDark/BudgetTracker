c77d568288c3f9b793c3ee4f45d3eeaf01e6090f
Kirill Orlov <i@kirillorlov.pro>
2020-02-10 01:59:57 +0300

    Fix JS codegen

--- BudgetTracker.Client/generate.js
+++ BudgetTracker.Client/generate.js
@@ -18,72 +18,88 @@ const protoDir = path.join(__dirname, "protoc");
 const protoc_bin = path.join(protoDir, "bin");
 const protoc = path.join(protoc_bin, "protoc" + platformExtension);
 
-if (!fs.existsSync(protoc)) {
-    console.log("Downloading protoc...");
-    if (!fs.existsSync(protoDir)) {
-        fs.mkdirSync(protoDir);
+function downloadProtoc(cb) {
+    if (!fs.existsSync(protoc)) {
+        console.log("Downloading protoc...");
+        if (!fs.existsSync(protoDir)) {
+            fs.mkdirSync(protoDir);
+        }
+        
+        var protoCreleases = {
+        "win32": `https://github.com/google/protobuf/releases/download/v${protoVersion}/protoc-${protoVersion}-win32.zip`,
+        "linux": `https://github.com/google/protobuf/releases/download/v${protoVersion}/protoc-${protoVersion}-linux-x86_64.zip`,
+        };
+    
+        request(protoCreleases[process.platform])
+        .pipe(unzip.Parse())
+        .on("entry", function(entry) {
+            var isFile = "File" === entry.type;
+            var isDir = "Directory" === entry.type;
+            var fullpath = path.join(protoDir, entry.path);
+            var directory = isDir ? fullpath : path.dirname(fullpath);
+    
+            if (!fs.existsSync(directory)) {
+                fs.mkdirSync(directory);
+            }
+    
+            if (isFile) {
+                entry.pipe(fs.createWriteStream(fullpath))
+                .on("finish", function() {
+                    if (protoc === fullpath) {
+                        fs.chmodSync(fullpath, 0o755);
+                    };
+                });
+            }
+        })
+        .on("close", cb);
+    } else {
+        cb();
+    }
+}
+
+function downloadProtoGrpc(cb) {
+    const genGrpcFullPath = path.join(protoc_bin, "protoc-gen-grpc-web" + platformExtension);
+    if (!fs.existsSync(genGrpcFullPath)) {
+        console.log("Downloading protoc-gen-grpc-web plugin...");
+        var protoCwebReleases = {
+            "win32": `https://github.com/grpc/grpc-web/releases/download/${protoWebVersion}/protoc-gen-grpc-web-${protoWebVersion}-windows-x86_64.exe`,
+            "linux": `https://github.com/grpc/grpc-web/releases/download/${protoWebVersion}/protoc-gen-grpc-web-${protoWebVersion}-linux-x86_64`
+        }
+    
+        request(protoCwebReleases[process.platform])
+        .pipe(fs.createWriteStream(genGrpcFullPath))
+        .on("close", () => {
+            fs.chmodSync(genGrpcFullPath, 0o755);
+            cb();
+        })
+    } else {
+        cb();
+    }
+}
+
+function generateCode() {
+    console.log("Generating code...");
+    const protofilesDir = path.join(__dirname, "../BudgetTracker.Protocol");
+    const outputDir = path.join(__dirname, "src/generated");
+    
+    if (fs.existsSync(outputDir)) {
+        rimraf.sync(outputDir);
     }
     
-    var protoCreleases = {
-    "win32": `https://github.com/google/protobuf/releases/download/v${protoVersion}/protoc-${protoVersion}-win32.zip`,
-    "linux": `https://github.com/google/protobuf/releases/download/v${protoVersion}/protoc-${protoVersion}-linux-x86_64.zip`,
-    };
-
-    request(protoCreleases[process.platform])
-    .pipe(unzip.Parse())
-    .on("entry", function(entry) {
-        var isFile = "File" === entry.type;
-        var isDir = "Directory" === entry.type;
-        var fullpath = path.join(protoDir, entry.path);
-        var directory = isDir ? fullpath : path.dirname(fullpath);
-
-        if (!fs.existsSync(directory)) {
-            fs.mkdirSync(directory);
+    fs.mkdirSync(outputDir);
+    
+    cp.execFile(protoc, 
+        [
+            "-I=" + protofilesDir,
+            "--js_out=import_style=commonjs:" + outputDir, 
+            "--grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:" + outputDir
+        ].concat(fs.readdirSync(protofilesDir)), 
+        { env: { "PATH": protoc_bin } }, 
+        function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
         }
-
-        if (isFile) {
-            entry.pipe(fs.createWriteStream(fullpath))
-            .on("finish", function() {
-                if (protoc === fullpath) {
-                    fs.chmodSync(fullpath, 0o755);
-                };
-            });
-        }
-    });
+    );
 }
 
-const genGrpcFullPath = path.join(protoc_bin, "protoc-gen-grpc-web" + platformExtension);
-if (!fs.existsSync(genGrpcFullPath)) {
-    console.log("Downloading protoc-gen-grpc-web plugin...");
-    var protoCwebReleases = {
-        "win32": `https://github.com/grpc/grpc-web/releases/download/${protoWebVersion}/protoc-gen-grpc-web-${protoWebVersion}-windows-x86_64.exe`,
-        "linux": `https://github.com/grpc/grpc-web/releases/download/${protoWebVersion}/protoc-gen-grpc-web-${protoWebVersion}-linux-x86_64`
-    }
-
-    request(protoCwebReleases[process.platform]).pipe(fs.createWriteStream(genGrpcFullPath))
-    fs.chmodSync(genGrpcFullPath, 0o755);
-}
-
-// --- ACTUAL GENERATION
-
-const protofilesDir = path.join(__dirname, "../BudgetTracker.Protocol");
-const outputDir = path.join(__dirname, "src/generated");
-
-if (fs.existsSync(outputDir)) {
-    rimraf.sync(outputDir);
-}
-
-fs.mkdirSync(outputDir);
-
-cp.execFile(protoc, 
-    [
-        "-I=" + protofilesDir,
-        "--js_out=import_style=commonjs:" + outputDir, 
-        "--grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:" + outputDir
-    ].concat(fs.readdirSync(protofilesDir)), 
-    { env: { "PATH": protoc_bin } }, 
-    function(err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    }
-);
+downloadProtoc(() => downloadProtoGrpc(() => generateCode()))
