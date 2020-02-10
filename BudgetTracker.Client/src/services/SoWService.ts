5957b6a9db7db81048490f536abfab136b6dbb78
Kirill Orlov <i@kirillorlov.pro>
2020-02-11 01:15:13 +0300

    Extracted reconnect logic

--- BudgetTracker.Client/src/services/SoWService.ts
+++ BudgetTracker.Client/src/services/SoWService.ts
@@ -1,22 +1,29 @@
-import proto from '../generated/StateOfTheWorld_grpc_web_pb';
+import proto, { SoWServiceClient } from '../generated/StateOfTheWorld_grpc_web_pb';
 import message from '../generated/StateOfTheWorld_pb';
 import { writable } from 'svelte/store';
+import { ClientReadableStream } from 'grpc-web';
 
 export class SoWService {
     Empty = new message.Empty()
     SystemInfo = writable(<any>null);
 
-    getSystemInfo() {
+    private reconnect<T>(method: (x: SoWServiceClient) => ClientReadableStream<T>, callback: (x: T) => any) {
         var svc = new proto.SoWServiceClient(document.location.protocol + '//' + document.location.host, null, null);
-        svc.getSystemInfo(this.Empty, undefined)
+        method(svc)
         .on("data", response => {
-            let object = response.toObject(false)
-            this.SystemInfo.set(object);
+            callback(response);
         })
-        .on("end", () => this.getSystemInfo())
+        .on("end", () => this.reconnect(method, callback))
         .on("error", err => {
             console.error(err);
-            setTimeout(() => this.getSystemInfo(), 1000);
+            setTimeout(() => this.reconnect(method, callback), 1000);
+        })
+    }
+
+    getSystemInfo() {
+        this.reconnect(x=>x.getSystemInfo(this.Empty, undefined), response => {
+            let object = response.toObject(false)
+            this.SystemInfo.set(object);
         })
     }
 
